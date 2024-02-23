c75e563937b7c48cc60c70c6a3629a27d9820102
ecschoye <ecschoye@stud.ntnu.no>
2024-02-23 21:38:05 +0100

    show result

--- src/pages/Roulette.js
+++ src/pages/Roulette.js
@@ -41,50 +41,72 @@ const Roulette = () => {
         { option: '3', style: { backgroundColor: 'red' } },
         { option: '26', style: { backgroundColor: 'black' } },
         
-      ]
+    ]
 
     const [mustSpin, setMustSpin] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
     const [rouletteData, setRouletteData] = useState(data);
+    const [showPrize, setShowPrize] = useState(false);
 
     const handleSpin = () => {
         const randomPrize = Math.floor(Math.random() * data.length);
         setPrizeNumber(randomPrize);
         setMustSpin(true);
+        setShowPrize(false);
         setTimeout(() => {
             setMustSpin(false);
+            setShowPrize(true);
         }, 5000); // Change the duration as per your requirement
     };
+
+    const prizeDisplayStyle = {
+        height: '50px', // Set this to the height of your prize display
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        visibility: showPrize ? 'visible' : 'hidden', // Hide the content but reserve the space
+        fontSize: '40px',
+    };
+
+    // Add media query for phones
+    
 
     return (
         <div className="App-body">
             <h1>Roulette</h1>
-            <p>Coming soon...</p>
-            <button onClick={handleSpin}>Spin</button>
-            <Wheel
-                mustStartSpinning={mustSpin}
-                spinDuration={[0.4]}
-                prizeNumber={prizeNumber}
-                data={rouletteData}
-                outerBorderColor={["black"]}
-                outerBorderWidth={[9]}
-                innerBorderColor={["Baige"]}
-                radiusLineColor={["pink"]}
-                radiusLineWidth={[1]}
-                textColors={["white"]}
-                textDistance={70}
-                fontSize={[10]}
-                innerRadius={40}
-                renderChild={(option, index, changeSegment) => (
-                    <div style={{ textAlign: 'center', fontSize: '16px' }}>
-                        <div>{option.option}</div>
-                        <div style={{ marginTop: '10px' }}>{index}</div>
+            <button onClick={handleSpin} style={{ fontSize: '20px', padding: '10px 20px' }}>Spin</button>
+
+                <div className="wheel-container">
+                    <Wheel
+                        mustStartSpinning={mustSpin}
+                        spinDuration={[0.4]}
+                        prizeNumber={prizeNumber}
+                        data={rouletteData}
+                        outerBorderColor={["black"]}
+                        outerBorderWidth={[9]}
+                        innerBorderColor={["Baige"]}
+                        radiusLineColor={["pink"]}
+                        radiusLineWidth={[1]}
+                        textColors={["white"]}
+                        textDistance={70}
+                        fontSize={[10]}
+                        innerRadius={40}
+                        renderChild={(option, index, changeSegment) => (
+                            <div style={{ textAlign: 'center', fontSize: '16px', color: 'white', backgroundColor: option.style.backgroundColor }}>
+                                <div>{option.option}</div>
+                                <div style={{ marginTop: '10px' }}>{index}</div>
+                            </div>
+                        )}
+                        onStopSpinning={() => {
+                            console.log("finish spinning");
+                        }}
+                    />
+                    {showPrize && (
+                    <div className="prize-display" style={prizeDisplayStyle}>
+                    <p>{rouletteData[prizeNumber].option}</p>
                     </div>
-                )}
-                onStopSpinning={() => {
-                    console.log("finish spinning");
-                }}
-            />
+                    )}
+                </div>
         </div>
     );
 };
