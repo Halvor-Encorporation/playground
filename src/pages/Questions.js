c106d392845441e70d401fb9029ce943af489ba6
ecschoye <ecschoye@stud.ntnu.no>
2024-02-23 21:58:39 +0100

    css

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -21,15 +21,16 @@ const Questions = () => {
     };
 
     return (
-        <div>
+        <div style={{ maxWidth: '60%', margin: '0 auto' }}>
             <h1>Velkommen! Skål alle sammen!</h1>
             {randomQuestions.map((question, index) => (
-                <div key={index}>
+                <div key={index} style={{ backgroundColor: index % 2 === 0 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0.06)', padding: '1px 0', borderRadius: '10px', marginBottom: '1px' }}>
                     {index % 10 === 0 && index !== 0 && <h2>{index} SPØRSMÅL! SKÅL!</h2>}
                     <p>{question}</p>
                 </div>
             ))}
             <h2>Game over. Chug drinken din!</h2>
+            <p>Dette gjelder spilleren som holder kasteobjektet</p>
             <button onClick={goBackHome}>Gå tilbake</button> {}
         </div>
     );
