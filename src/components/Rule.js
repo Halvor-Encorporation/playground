6051eb2683d62deda9f5a69e3d54cf7454acdc4c
ecschoye <ecschoye@stud.ntnu.no>
2024-02-24 10:52:20 +0100

    styling

--- src/components/Rule.js
+++ src/components/Rule.js
@@ -2,10 +2,13 @@
 import React from 'react';
 
 // Component for displaying an individual rule
-const Rule = ({ number, text }) => {
-  return (
-    <p><b>Rule {number}:</b> {text}</p>
-  );
-};
-
+const Rule = ({ number, text, backgroundColor }) => {
+    // Apply the backgroundColor prop to the paragraph style
+    return (
+      <div style={{ backgroundColor: backgroundColor, padding: '10px', borderRadius: '10px', marginBottom: '5px' }}>
+        <p><b>Rule {number}:</b> {text}</p>
+      </div>
+    );
+  };
+  
 export default Rule;
--- src/pages/Sveinung.js
+++ src/pages/Sveinung.js
@@ -13,6 +13,10 @@ const SveinungGameRules = () => {
     "Når alle har passet eller byttet, viser alle kortene. Den med lavest verdi drikker slurker lik kortets tall (bildekort = 10 slurker, ess = 1 slurk). Ved lik lavest verdi mellom to eller flere spillere, drikkes dobbelt så mange slurker."
   ];
 
+  const getBackgroundColor = (index) => {
+    return index % 2 === 0 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0.06)';
+  };
+
   return (
     <div>
       <h1>Sveinung's drikkelek</h1>
@@ -20,10 +24,11 @@ const SveinungGameRules = () => {
         <img src={gameImage} alt="Game illustration" className="gameImage" style={{ borderRadius: '90%', height: 'auto' }} />
       </div>
       <h2>Anbefalt spillere: 4-9 personer</h2>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div style={{ width: '60%' }}>
+      {/* Apply the 'question-container' class to the div that wraps the rules */}
+      <div className="question-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div>
           {rules.map((rule, index) => (
-            <Rule key={index} number={index + 1} text={rule} />
+            <Rule key={index} number={index + 1} text={rule} backgroundColor={getBackgroundColor(index)} />
           ))}
         </div>
       </div>
